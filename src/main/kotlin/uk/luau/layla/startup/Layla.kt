5f19bf6ae57dc9594a07bcda2773e78c5bd7fdfd
Sam Pritchard <sampritchard0801@gmail.com>
2022-07-20 01:58:39 +0100

    Add an example ping command.

--- src/main/kotlin/uk/luau/layla/startup/Layla.kt
+++ src/main/kotlin/uk/luau/layla/startup/Layla.kt
@@ -1,11 +1,35 @@
 package uk.luau.layla.startup
 
 import dev.kord.core.Kord
+import dev.kord.core.behavior.interaction.response.respond
+import dev.kord.core.event.interaction.GlobalChatInputCommandInteractionCreateEvent
+import dev.kord.core.on
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
+import kotlin.system.measureTimeMillis
+import kotlin.time.ExperimentalTime
 
 suspend fun main() {
     val kord = Kord(System.getenv("LAYLA_TOKEN"))
+
+    kord.createGlobalChatInputCommand("ping", "Fetches connectivity information between Discord and Layla.") { }
+
+    @OptIn(ExperimentalTime::class)
+    kord.on<GlobalChatInputCommandInteractionCreateEvent> {
+        val response = interaction.deferPublicResponse()
+        val latency = measureTimeMillis {
+            kord.rest.user.getCurrentUser()
+        }
+
+        var content = "Discord API Latency: **${latency}ms**\n"
+        kord.gateway.averagePing?.let {
+            content += "Discord Gateway Ping: **${it.inWholeMilliseconds}ms**"
+        }
+
+        response.respond {
+            this.content = content
+        }
+    }
 
     kord.login {
         @OptIn(PrivilegedIntent::class)
