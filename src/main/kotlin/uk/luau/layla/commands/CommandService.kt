ae22ee2ed6499880849590ad833f1b6b0afb7aa3
Sam Pritchard <sampritchard0801@gmail.com>
2022-07-23 04:40:17 +0100

    Clean up CommandService file a little

--- src/main/kotlin/uk/luau/layla/commands/CommandService.kt
+++ src/main/kotlin/uk/luau/layla/commands/CommandService.kt
@@ -12,28 +12,28 @@ class CommandService : KoinComponent {
     private val commandMap by inject<MutableMap<String, Command>>()
     private val layla by inject<Layla>()
 
-    fun command(name: String) = commandMap[name]
+    fun setupEventHandlers() {
+        layla.kord.on<GuildChatInputCommandInteractionCreateEvent> {
+            commandMap[interaction.invokedCommandName]?.executeAsGuild(this)
+        }
+
+        layla.kord.on<ChatInputCommandInteractionCreateEvent> {
+            commandMap[interaction.invokedCommandName]?.execute(this)
+        }
+    }
 
     suspend fun command(name: String, description: String, setup: ChatInputCreateBuilder.() -> Unit = {},
                         execute: suspend GuildChatInputCommandInteractionCreateEvent.() -> Unit = {}) {
 
         layla.kord.createGlobalChatInputCommand(name, description, setup)
+
         commandMap[name] = object : Command(name, description, setup) {
             override suspend fun executeAsGuild(context: GuildChatInputCommandInteractionCreateEvent) = execute(context)
         }
     }
-
-    fun setupEventHandlers() {
-        layla.kord.on<GuildChatInputCommandInteractionCreateEvent> {
-            command(interaction.invokedCommandName)?.executeAsGuild(this)
-        }
-
-        layla.kord.on<ChatInputCommandInteractionCreateEvent> {
-            command(interaction.invokedCommandName)?.execute(this)
-        }
-    }
 }
 
+@Suppress("unused") // Just to stop getting bothered about name/description not being referenced yet. ;w;
 open class Command(val name: String, val description: String,
                    private val builder: ChatInputCreateBuilder.() -> Unit = {}) {
 
